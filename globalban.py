79f3f012f2b0ae6093623106c7e6405615d15594
Bluscream <admin@timo.de.vc>
2017-06-29 09:31:17 +0200

    fixed notraces+added some moah globalban crap

--- globalban.py
+++ globalban.py
@@ -1,4 +1,4 @@
-import discord, asyncio
+import discord, asyncio, requests
 from discord.ext import commands
 from cogs.utils.checks import *
 
@@ -16,12 +16,44 @@ class globalban:
                 member = server.get_member(int(user))
                 if member: return member
 
-            client = discord.Client()
+            """client = discord.Client()
+
             @client.event
             async def on_ready():
-                return client.get_user_info(int(user))
+              print('Logged in as')
+              print(client.user.name)
+              print(client.user.id)
+              print('------')
+
             client.run('MzI5MjI2NTA2MTEzMTIyMzA0.DDPXnw.UMN_crUM-UX2233jnXmCvyP55cc')
-        except: pass
+            return client.get_user_info(int(user))"""
+            return False
+        except:
+            pass
+
+    def block_user(self, id):
+        payload = {
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.157 Chrome/58.0.3029.110 Discord Canary/1.7.1 Safari/537.36',
+            # 'authority': 'canary.discordapp.com',
+            'authorization': 'MzI5MjI2NTA2MTEzMTIyMzA0.DDPXnw.UMN_crUM-UX2233jnXmCvyP55cc'
+        }
+        r = requests.put("https://canary.discordapp.com/api/v6/users/@me/relationships/"+id, data=payload)
+        msg = "Requested https://canary.discordapp.com/api/v6/users/@me/relationships/"+id
+        msg += "\nStatus: %s" % r.status_code
+        msg += "\nContent: %s" % r.content
+        return msg
+
+    def unblock_user(self, id):
+        payload = {
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.157 Chrome/58.0.3029.110 Discord Canary/1.7.1 Safari/537.36',
+            # 'authority': 'canary.discordapp.com',
+            'authorization': 'MzI5MjI2NTA2MTEzMTIyMzA0.DDPXnw.UMN_crUM-UX2233jnXmCvyP55cc'
+        }
+        r = requests.delete("https://canary.discordapp.com/api/v6/users/@me/relationships/"+id, data=payload)
+        msg = "Requested https://canary.discordapp.com/api/v6/users/@me/relationships/"+id
+        msg += "\nStatus: %s" % r.status_code
+        msg += "\nContent: %s" % r.content
+        return msg
 
     @commands.command(aliases=['fu'], pass_context=True)
     async def finduser(self, ctx, *, user):
@@ -32,12 +64,22 @@ class globalban:
         except:
             from traceback import format_exc;await self.bot.send_message(ctx.message.channel, self.bot.bot_prefix + " Error:\n\n%s" % format_exc())
 
+    @commands.command(aliases=['bl'], pass_context=True)
+    async def block(self, ctx, *, user):
+        try:
+            member = self.get_user(user,ctx.message)
+            block = self.block_user(member.id)
+            await self.bot.send_message(ctx.message.channel, block)
+        except:
+            from traceback import format_exc;await self.bot.send_message(ctx.message.channel, self.bot.bot_prefix + " Error:\n\n%s" % format_exc())
+
     @commands.command(aliases=['gban'], pass_context=True)
     async def globalban(self, ctx, *, user):
         try:
             member = self.get_user(user,ctx.message)
             if not member: await self.bot.send_message(message.channel, "User \"%s\" not found."%user); return
             servers = 0
+            # TODO Blocking
             for server in self.bot.servers:
                 try:
                     await self.bot.ban(member, delete_message_days=7)
--- notraces.py
+++ notraces.py
@@ -1,4 +1,5 @@
 import discord, asyncio
+from discord.ext import commands
 from cogs.utils.checks import *
 
 class notraces:
@@ -11,22 +12,22 @@ class notraces:
         self.cmd_prefix = config["cmd_prefix"]
         self.bot_prefix = config["bot_identifier"]
     
-    @commands.command(aliases=['fu'], pass_context=True)
-    async def finduser(self, ctx):
-        if message.content.startswith("{p}notrace".format(p=self.cmd_prefix)):
-            params = message.content.split(' ', 1)
-            await self.bot.delete_message(message)
+    @commands.command(aliases=['nt'], pass_context=True)
+    async def notrace(self, ctx):
+        if not self.active:
+        #if ' ' in ctx.message.content:
+            params = ctx.message.content.split(' ', 1)
             if len(params) == 2: self.delete_after = int(params[1].strip())
-            await self.bot.send_message(message.channel, "{p} No Traces mode activated, all following messages will auto-delete after {t} secs.".format(p=self.bot_prefix,t=self.delete_after))
+            await self.bot.send_message(ctx.message.channel, "{p} No Traces mode activated, all following messages will auto-delete after {t} secs.".format(p=self.bot_prefix,t=self.delete_after))
             self.active = True
-        elif message.content == "{p}trace".format(p=self.cmd_prefix):
-            await self.bot.delete_message(message)
-            await self.bot.send_message(message.channel, "{p} No Traces mode deactivated.".format(p=self.bot_prefix))
+        else:
+            await self.bot.send_message(ctx.message.channel, "{p} No Traces mode deactivated.".format(p=self.bot_prefix))
             self.active = False
-        elif self.active:
+
+    async def on_message(self, message):
+        if self.active and message.author == self.bot.user:
             await asyncio.sleep(self.delete_after)
             await self.bot.delete_message(message)
-        return True
-    
+
 def setup(bot):
     bot.add_cog(notraces(bot))
