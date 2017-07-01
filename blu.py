53f3b2145554c41c5e41abe36efa75d78867d9ae
Bluscream <admin@timo.de.vc>
2017-07-01 15:19:42 +0200

    added möpse

--- blu.py
+++ blu.py
@@ -5,12 +5,18 @@ class blu:
 
     def __init__(self, bot):
         self.bot = bot
-        config = load_config()
-        self.bot_prefix = config["bot_identifier"]
     
     async def on_message(self, message):
-        if message.content == "plu":
-            await self.bot.send_message(message.channel, "is garnicht da {p}".format(p=self.bot_prefix))
-    
+        if message.content.lower() in ['plu','blu'] and message.server.id == '295183533423591435':
+            await self.bot.send_message(message.channel, "is garnicht da {p}".format(p=self.bot.bot_prefix))
+        if message.content.lower() == 'möp':
+            if message.author.permissions_in(message.channel).add_reactions:
+                await self.bot.add_reaction(message, '\N{REGIONAL INDICATOR SYMBOL LETTER S}')
+                await self.bot.add_reaction(message, '\N{REGIONAL INDICATOR SYMBOL LETTER E}')
+                if message.author == self.bot.user:
+                    await asyncio.sleep(30)
+                    await self.bot.remove_reaction(message, '\N{REGIONAL INDICATOR SYMBOL LETTER S}', self.bot.user)
+                    await self.bot.remove_reaction(message, '\N{REGIONAL INDICATOR SYMBOL LETTER E}', self.bot.user)
+
 def setup(bot):
     bot.add_cog(blu(bot))
