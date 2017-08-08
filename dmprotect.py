1516c2d49b4c056d0c6ef058405208b02ac110a5
LyricLy <hanson.twentytwo@gmail.com>
2017-08-09 10:23:43 +1200

    Improvements

--- dmprotect.py
+++ dmprotect.py
@@ -1,25 +1,29 @@
 import discord, asyncio, datetime
 from discord.ext import commands
 from cogs.utils.checks import *
-from cogs.utils.api import block_user
 
-class dmprotect:
-    active = False
-    allowed = False
-    invites = ['discord.gg', 'discord.io', 'discordapp.com/invite']
-    def __init__(self, bot):
+class DMProtect:
+    def __init__(self):
+        self.active = False
+        self.allowed = False
+        self.invites = ['discord.gg', 'discord.io', 'discordapp.com/invite']
         self.bot = bot
 
     async def on_message(self, message):
-        if not self.active: return
-        if not isinstance(message.channel, discord.abc.PrivateChannel): return
-        if self.allowed: self.allowed = False; return
+        if not self.active: 
+            return
+        if not isinstance(message.channel, discord.abc.PrivateChannel): 
+            return
+        if self.allowed: 
+            self.allowed = False
+            return
         messages = await message.channel.history(limit=2).flatten()
-        if not len(messages) == 1: return print('not first message')
+        if not len(messages) == 1: 
+            return
         for invite in self.invites:
             if invite in message.content:
-                block_user(message.author.id)
-                print('User %s#%s used \"%s\" in first private message, therefor i blocked him for you!'%(message.author.name, message.author.discriminator, invite))
+                message.author.block()
+                print('Automatically blocked user {} for sending an invite link ({}) in their first private message to you.'.format(str(message.author), invite))
                 return
 
     @commands.command(aliases=['dmp'], pass_context=True)
@@ -28,7 +32,7 @@ class dmprotect:
         await ctx.message.delete()
         self.active = not self.active
         self.allowed = True
-        await ctx.message.channel.send( self.bot.bot_prefix + 'DM Protection set to: `%s`' % self.active)
+        await ctx.send(self.bot.bot_prefix + 'DM Protection set to: `{}`'.format(self.active))
 
 def setup(bot):
-    bot.add_cog(dmprotect(bot))
+    bot.add_cog(DMProtect(bot))
